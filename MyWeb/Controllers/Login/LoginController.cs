c9e68424c28d262befc6a8cf7e2cf718cdaf21e7
Matteo <MatteoMoro@ICHI-PC-DEV-003>
2024-11-05 12:28:26 +0100

    Initial data

---
+++ MyWeb/Controllers/Login/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWeb.Controllers.Login
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        public LoginController()
+        {
+
+        }
+
+        [HttpPost]
+        public void Login()
+        {
+
+        }
+
+        [HttpPost]
+        public void LogOut()
+        {
+
+        }
+    }
+}
---
+++ MyWeb/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
+builder.Configuration.AddJsonFile(".\\AppSettings.json");
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+WebApplication app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+}); 
+app.MapControllers();
+
+app.Run(builder.Configuration.GetValue<string>("ApplicationUrl"));
